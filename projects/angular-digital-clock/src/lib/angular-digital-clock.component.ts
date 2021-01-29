dcdf7bd151a9ba55a58683272ec26f202b4331cb
izhostov <ivan.zhostov@techture.io>
2021-01-29 13:36:11 +0200

    add default format for hour and minute

--- projects/angular-digital-clock/src/lib/angular-digital-clock.component.ts
+++ projects/angular-digital-clock/src/lib/angular-digital-clock.component.ts
@@ -1,21 +1,20 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormatType } from '../enums/formatType';
-import { PeriodType } from '../enums/periodType';
-import { TimeFormat } from '../enums/timeFormat';
+import { Component, Input, OnInit } from "@angular/core";
+import { FormatType } from "../enums/formatType";
+import { PeriodType } from "../enums/periodType";
+import { TimeFormat } from "../enums/timeFormat";
 
 @Component({
-  selector: 'lib-angular-digital-clock',
-  templateUrl: './angular-digital-clock.component.html',
-  styleUrls: ['./angular-digital-clock.component.scss']
+  selector: "lib-angular-digital-clock",
+  templateUrl: "./angular-digital-clock.component.html",
+  styleUrls: ["./angular-digital-clock.component.scss"],
 })
 export class AngularDigitalClockComponent implements OnInit {
+  @Input()
+  hourFormat;
 
   @Input()
-  hourFormat: string;
+  minuteFormat;
 
-  @Input()
-  minuteFormat: string;
-  
   date = new Date();
 
   public get hour() {
@@ -33,29 +32,31 @@ export class AngularDigitalClockComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
+    this.hourFormat = "h";
+    this.minuteFormat = "m";
     this.getCurrentDate();
   }
 
   getCurrentDate() {
-    setInterval(() => {
-      this.date = new Date();
-    }, 1000);
+    // setInterval(() => {
+    //   this.date = new Date();
+    // }, 1000);
   }
 
   onFormatSlected(time) {
-    time.formatType === FormatType.Hour ? 
-    this.hourFormat = time.format :
-    this.minuteFormat = time.format;
+    time.formatType === FormatType.Hour
+      ? (this.hourFormat = time.format)
+      : (this.minuteFormat = time.format);
   }
 
   isTwelveHourFormat() {
     if (
-      (this.hourFormat === TimeFormat.LeadingZero12Hours ||
-      this.hourFormat === TimeFormat.NoLeadingZero12Hours)
+      this.hourFormat === TimeFormat.LeadingZero12Hours ||
+      this.hourFormat === TimeFormat.NoLeadingZero12Hours
     ) {
       return this.date.getHours() >= 12 ? PeriodType.PM : PeriodType.AM;
     } else {
-      return '';
+      return "";
     }
   }
 }
